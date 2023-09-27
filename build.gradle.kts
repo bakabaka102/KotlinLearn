14c2aec4c650a294efa05a7fa91636efad50cb8e
Han Ng <H Ng>
2023-09-28 00:15:56 +0700

    Update gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,18 +16,18 @@ repositories {
 
 dependencies {
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     // Because RxAndroid releases are few and far between, it is recommended you also
     // explicitly depend on RxJava's latest version for bug fixes and new features.
     // see https://github.com/ReactiveX/RxJava/releases for latest 3.x.x version
-    implementation("io.reactivex.rxjava3:rxjava:3.0.2")
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.6")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
 
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
 }
 
 tasks.test {
