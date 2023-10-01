4330aecd96213d6513e76512f19e9c3068e6c2a7
Han Ng <H Ng>
2023-10-01 16:22:39 +0700

    - Builder pattern

---
+++ src/main/kotlin/designpattern/builder_pattern/CarBuilder.kt
@@ -0,0 +1,39 @@
+package designpattern.builder_pattern
+
+class CarBuilder : IBuilder {
+
+    private var type: CarType? = null
+    private var seats = 0
+    private var engine: Engine? = null
+    private var transmission: Transmission? = null
+    private var tripComputer: TripComputer? = null
+    private var gpsNavigator: GPSNavigator? = null
+
+    fun getResult(): Car {
+        return Car(type, seats, engine, transmission, tripComputer, gpsNavigator)
+    }
+
+    override fun setCarType(type: CarType?) {
+        this.type = type
+    }
+
+    override fun setSeats(seats: Int) {
+        this.seats = seats
+    }
+
+    override fun setEngine(engine: Engine?) {
+        this.engine = engine
+    }
+
+    override fun setTransmission(transmission: Transmission?) {
+        this.transmission = transmission
+    }
+
+    override fun setTripComputer(tripComputer: TripComputer?) {
+        this.tripComputer = tripComputer
+    }
+
+    override fun setGPSNavigator(gpsNavigator: GPSNavigator?) {
+        this.gpsNavigator = gpsNavigator
+    }
+}
---
+++ src/main/kotlin/designpattern/builder_pattern/Director.kt
@@ -0,0 +1,41 @@
+package designpattern.builder_pattern
+
+fun IBuilder.constructSportsCar() {
+    this.setCarType(CarType.SPORTS_CAR)
+    this.setSeats(2)
+    this.setEngine(Engine(3.0, 0.0))
+    this.setTransmission(Transmission.SEMI_AUTOMATIC)
+    this.setTripComputer(TripComputer())
+    this.setGPSNavigator(GPSNavigator())
+}
+
+fun IBuilder.constructCityCar() {
+    this.setCarType(CarType.CITY_CAR)
+    this.setSeats(2)
+    this.setEngine(Engine(1.2, 0.0))
+    this.setTransmission(Transmission.AUTOMATIC)
+    this.setTripComputer(TripComputer())
+    this.setGPSNavigator(GPSNavigator())
+}
+
+fun IBuilder.constructSUV() {
+    this.setCarType(CarType.SUV)
+    this.setSeats(4)
+    this.setEngine(Engine(2.5, 0.0))
+    this.setTransmission(Transmission.MANUAL)
+    this.setGPSNavigator(GPSNavigator())
+}
+
+fun main() {
+    val manualBuilder = CarManualBuilder()
+    // Director may know several building recipes.
+    manualBuilder.constructSportsCar()
+    val carManual = manualBuilder.getResult()
+    println("Car manual built:  $carManual")
+
+    val carBuilder = CarBuilder()
+    // Director may know several building recipes.
+    carBuilder.constructSportsCar()
+    val car = carBuilder.getResult()
+    println("Car built:  $car")
+}
